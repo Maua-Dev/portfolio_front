d1d2956d599314be3528703f159436b690aa9d6b
tokujiTO <tiagomassuda123@gmail.com>
2025-08-14 17:30:06 -0300

    feat: add react with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-[100vh] bg-red-200 w-full flex justify-center items-center">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
