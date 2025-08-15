212799f10225fa671ee03ec951515598c0edf190
tokujiTO <tiagomassuda123@gmail.com>
2025-08-15 10:11:58 -0300

    feat: add project component finished

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -17,23 +17,32 @@ interface ProjectProps {
 
 export default function Project({ project, side }: ProjectProps) {
   return (
-    <div className="bg-red-300 relative w-full h-[80vh] flex items-center justify-center mt-4">
-      <div className="flex w-1/2 h-full flex-col">
-        <img src={project.cellImage} alt="" />
+    <div
+      className={cn(
+        "relative w-full h-[80vh] flex items-center justify-center mt-4 px-8",
+        {
+          "flex-row-reverse": side === "right",
+        },
+      )}
+    >
+      <div className="flex w-1/2 z-10 h-full flex-col">
+        <img src={project.cellImage} alt="" className="h-full w-auto" />
       </div>
-      <div className="flex w-1/2 h-full flex-col items-center justify-center z-10 font-poppins text-coolWhite">
-        <img src={project.image} alt="" />
-        <h1 className="text-4xl">Tecnologias</h1>
-        <p className="text-2xl">Frontend: {project.frontend}</p>
-        <p className="text-2xl">Backend: {project.backend}</p>
-        <p className="text-xl">{project.description}</p>
-        <button className="rounded-full px-4 py-2 bg-transparent border-2 border-coolWhite  hover:bg-coolWhite hover:text-black transition-colors duration-300 ">
-          <p>Saiba Mais</p>
-        </button>
+      <div className="flex w-1/2 h-full flex-col gap-4 items-center z-10 font-poppins text-coolWhite">
+        <img src={project.image} alt="" className="h-2/6 w-auto mb-1/6" />
+        <div className="flex flex-col items-start mt-10 w-1/2">
+          <h1 className="text-5xl font-bold">Tecnologias</h1>
+          <p className="text-xl mt-2">Frontend: {project.frontend}</p>
+          <p className="text-xl mt-2">Backend: {project.backend}</p>
+          <p className="text-sm mt-8 text-justify">{project.description}</p>
+          <button className="mt-8 mx-auto hover:cursor-pointer rounded-full px-4 py-2 bg-transparent border-2 border-coolWhite  hover:bg-coolWhite hover:text-black transition-colors duration-300 ">
+            <p>Saiba Mais</p>
+          </button>
+        </div>
       </div>
       <div
         className={cn(
-          `absolute h-5/6 w-10/11 bottom-0 z-0`,
+          `absolute h-[64%] w-10/11 bottom-0 z-0`,
           side === "left" ? "left-0 rounded-r-xl" : "right-0 rounded-l-xl",
         )}
         style={{ backgroundColor: project.color }}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,37 @@
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import Project from "../components/project";
+import cell1 from "../assets/cellImage1.png";
+import img1 from "../assets/image1.png";
+import cell2 from "../assets/cellImage2.png";
+import img2 from "../assets/image2.png";
 
 const project = {
   id: "1",
   title: "Project 1",
-  description: "Description for Project 1",
-  image: "../assets/image1.jpg",
-  cellImage: "../assets/cellImage1.jpg",
+  description:
+    "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  image: img1,
+  cellImage: cell1,
   frontend: "React",
   backend: "Node.js",
-  color: "#ff0000",
+  color: "#AE0404E5",
 };
 const project2 = {
   id: "2",
   title: "Project 2",
-  description: "Description for Project 2",
-  image: "../assets/image2.jpg",
-  cellImage: "../assets/cellImage2.jpg",
+  description:
+    "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  image: img2,
+  cellImage: cell2,
   frontend: "React",
   backend: "Node.js",
-  color: "#0000ff",
+  color: "#0080F591",
 };
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-coolWhite">
       <Navbar />
       <h1>Welcome to Our Portfolio</h1>
       <Project project={project} side={"left"} />
