8f13595fa6f556dff4fa0e7cd4166b65a997d906
tokujiTO <tiagomassuda123@gmail.com>
2025-08-17 11:53:15 -0300

    feat: add test parallax

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,4 +1,6 @@
+import { motion, MotionValue, useScroll, useTransform } from "motion/react";
 import { cn } from "../utils/cn";
+import { useRef } from "react";
 
 interface Project {
   id: string;
@@ -10,26 +12,49 @@ interface Project {
   backend: string;
   color: string;
 }
+
 interface ProjectProps {
   project: Project;
   side: string;
 }
 
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, 1.9 * distance]);
+}
+
 export default function Project({ project, side }: ProjectProps) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+
   return (
-    <div
+    <motion.div
       className={cn(
         "relative w-full h-[80vh] flex items-center justify-center mt-4 px-8",
         {
           "flex-row-reverse": side === "right",
         },
       )}
+      ref={ref}
     >
       <div className="flex w-1/2 z-10 h-full flex-col">
-        <img src={project.cellImage} alt="" className="h-full w-auto" />
+        <motion.img
+          src={project.cellImage}
+          alt=""
+          initial={{
+            translateX: side === "right" ? 100 : -100,
+            visibility: "hidden",
+          }}
+          style={{ y: useParallax(scrollYProgress, 100) }}
+          whileInView={{ translateX: 0, visibility: "visible" }}
+          className="h-full w-auto"
+        />
       </div>
       <div className="flex w-1/2 h-full flex-col gap-4 items-center z-10 font-poppins text-coolWhite">
-        <img src={project.image} alt="" className="h-2/6 w-auto mb-1/6" />
+        <motion.img
+          src={project.image}
+          alt=""
+          className="h-2/6 w-auto mb-1/6"
+        />
         <div className="flex flex-col items-start mt-10 w-1/2">
           <h1 className="text-5xl font-bold">Tecnologias</h1>
           <p className="text-xl mt-2">Frontend: {project.frontend}</p>
@@ -40,13 +65,16 @@ export default function Project({ project, side }: ProjectProps) {
           </button>
         </div>
       </div>
-      <div
+      <motion.div
         className={cn(
-          `absolute h-[64%] w-10/11 bottom-0 z-0`,
+          `absolute h-[80%] w-10/11 bottom-0 z-0`,
           side === "left" ? "left-0 rounded-r-xl" : "right-0 rounded-l-xl",
         )}
-        style={{ backgroundColor: project.color }}
+        style={{
+          y: useParallax(scrollYProgress, 300),
+          backgroundColor: project.color,
+        }}
       />
-    </div>
+    </motion.div>
   );
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,79 @@
+import Project from "../components/project";
+import cell1 from "../assets/cellImage1.png";
+import img1 from "../assets/image1.png";
+import cell2 from "../assets/cellImage2.png";
+import img2 from "../assets/image2.png";
+import { useTransform, MotionValue, motion, useScroll } from "motion/react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import { useRef } from "react";
+
+const projects = [
+  {
+    id: "1",
+    title: "Project 1",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: img1,
+    cellImage: cell1,
+    frontend: "React",
+    backend: "Node.js",
+    color: "#AE0404E5",
+  },
+  {
+    id: "2",
+    title: "Project 2",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: img2,
+    cellImage: cell2,
+    frontend: "React",
+    backend: "Node.js",
+    color: "#0080F591",
+  },
+  {
+    id: "1",
+    title: "Project 1",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: img1,
+    cellImage: cell1,
+    frontend: "React",
+    backend: "Node.js",
+    color: "#AE0404E5",
+  },
+  {
+    id: "2",
+    title: "Project 2",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: img2,
+    cellImage: cell2,
+    frontend: "React",
+    backend: "Node.js",
+    color: "#0080F591",
+  },
+];
+
 export default function Projects() {
+
   return (
-    <div>
-      <h1>Our Projects</h1>
-      <p>This is the projects page.</p>
+    <div className="flex flex-col overflow-x-hidden overflow-y-hidden justify-center gap-80 bg-coolWhite min-h-screen">
+      <Navbar />
+      {projects.map((project, index) => (
+        <motion.div
+          key={project.id}
+          initial={{ visibility: "hidden", translateY: 100 }}
+          animate={{ visibility: "visible", translateY: 0 }}
+          className="w-full h-[80vh] flex items-center justify-center"
+        >
+          <Project
+            project={project}
+            side={index % 2 === 0 ? "left" : "right"}
+          />
+        </motion.div>
+      ))}
+      <Footer />
     </div>
   );
 }
