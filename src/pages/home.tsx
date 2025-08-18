3131aa848464be96187e1e44af1e3a2c0476848e
tokujiTO <tiagomassuda123@gmail.com>
2025-08-18 14:42:30 -0300

    feat: fix homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,11 @@
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
-import Project from "../components/project";
-import cell1 from "../assets/cellImage1.png";
-import img1 from "../assets/image1.png";
-import cell2 from "../assets/cellImage2.png";
-import img2 from "../assets/image2.png";
-
-const project = {
-  id: "1",
-  title: "Project 1",
-  description:
-    "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  image: img1,
-  cellImage: cell1,
-  frontend: "React",
-  backend: "Node.js",
-  color: "#AE0404E5",
-};
-const project2 = {
-  id: "2",
-  title: "Project 2",
-  description:
-    "lorem ipsum dolor sit amet, consectetur adipiscing elit. lorem lorem lorem lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  image: img2,
-  cellImage: cell2,
-  frontend: "React",
-  backend: "Node.js",
-  color: "#0080F591",
-};
 
 export default function Home() {
   return (
-    <div className="bg-coolWhite">
+    <div className="bg-coolWhite flex flex-col justify-between items-center min-h-[100vh]">
       <Navbar />
       <h1>Welcome to Our Portfolio</h1>
-      <Project project={project} side={"left"} />
-      <Project project={project2} side={"right"} />
       <p>This is the home page.</p>
       <Footer />
     </div>
