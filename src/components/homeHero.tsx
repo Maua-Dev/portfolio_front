92461d0be8e56f87a957e9b6cde75377e823d75c
Pedro-R-Vieira <priccomi.devmaua@gmail.com>
2025-08-27 20:28:15 -0300

    feat: add homeHero

---
+++ src/components/homeHero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomeHero: React.FC = () => {
+  return (
+    <section className="flex justify-between items-center bg-gray-100 px-20 py-32 gap-x-16 flex-wrap">
+      <div className="max-w-xl">
+        <h1 className="text-5xl leading-tight font-bold text-[#2b2b2b]">
+          Soluções ágeis <br />
+          criadas por <span className="text-[#4b0000] font-extrabold">quem vive</span> <br />
+          tecnologia.
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default HomeHero;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import Footer from "../components/footer";
+import HomeHero from "../components/homeHero";
 import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <div className="bg-coolWhite flex flex-col min-h-screen">
       <Navbar />
+      <HomeHero />
       <main className="flex-grow flex flex-col items-center justify-center">
         <h1>Welcome to Our Portfolio</h1>
         <p>This is the home page.</p>
